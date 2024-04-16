da961355599e42d2b6ab767e814f822a53e44052
Arthur-Ruas <arthurruasfe@gmail.com>
2024-04-16 20:31:31 -0300

    Arthur: Método Create e Get dos livros

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
+const livrolist = document.getElementById('book-list');
+
+function renderBook(doc){
+    let li = document.createElement('li');
+    let titulo = document.createElement('span');
+    let autor = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    titulo.textContent = doc.data().titulo;
+    autor.textContent = doc.data().autor;
+    
+    li.appendChild(titulo);
+    li.appendChild(autor);
+
+    livrolist.appendChild(li);
+}
+
 db.collection('libri-firestore')
-    .get()
-    .then(
-        (snapshot) =>{
-            snapshot.docs.forEach(doc => {
-                console.log(doc.data())
-            });
-        }
-    )
+.get()
+.then(
+    (snapshot) =>{
+        snapshot.docs.forEach(doc => {
+            console.log(doc.data());
+            renderBook(doc)
+        });
+    }
+)
+
+const form = document.getElementById("add-book-form")
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    db.collection('libri-firestore').add({
+        autor: form.autor.value,
+        titulo: form.titulo.value
+    }).then(() =>{
+        form.autor.value = '';
+        form.titulo.value = '';
+        window.location.reload();
+    })
+});
